f7109850daccd4f4596e1bcbe72acf6dedf1efaf
Jos <jjovito33@gmail.com>
2025-11-25 22:08:22 +0700

    optimize import

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Hero from './components/Hero'
 import About from './components/About'
+import Contact from './components/Contact'
+import Experience from './components/Experience'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import Contact from './components/Contact'
-import Navbar from './components/Navbar'
-import Experience from './components/Experience'
-import Footer from './components/Footer'
 
 export default function Home() {
   return (
